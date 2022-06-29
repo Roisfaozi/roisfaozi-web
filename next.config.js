f4bf14ad3163d96979c33359a9c733eaacdbc13b
Rois Faozi <roisfaozi55@gmail.com>
2022-06-29 23:58:53 +0700

    init project setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,30 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
+  purge: [
+    './pages/**/*.tsx',
+    './pages/**/*.js',
+    './pages/**/*.ts',
+    './components/**/*.tsx',
+    './components/**/*.js',
+    './components/**/*.ts',
+  ],
+
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['image.tmdb.org'],
+    formats: ['image/avif', 'image/webp'],
+  },
   pwa: {
     dest: 'public',
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
     runtimeCaching,
+    dynamicStartUrl: false,
   },
+  reactStrictMode: true,
 })
