27dde0a8495bc90d6d0bacf3e3278d8ba53cc82a
Rois Faozi <roisfaozi55@gmail.com>
2022-07-01 04:22:48 +0700

    add story compoent

---
+++ components/story.jsx
@@ -0,0 +1,26 @@
+export default function Story() {
+  return (
+    <section className='section section-container'>
+      <h2 className='section-title'>Story</h2>
+      <div className='story-section'>
+        <div className='w-1/3'></div>
+        <p className='intro'>
+          Hi! I'm Rois Faozi. I have a strong passion for technology. I
+          specialize in Front End Development and my passion is all about
+          building elegant and professional user interfaces and websites.
+          <br /> <br /> I live in the Indonesia. I'm interested in web
+          development since 10 years, and always find out about web development.
+          It fascinated me and it hooked me on, so I decided to pursue this
+          career. Started 3 years ago I decided to move my career as a Frontend
+          developer after 5 years as a designer. In development I'm passionate
+          to bring both the technical aspects and the visual aesthetic. User
+          experience, accessibility with good functionalityis important to me.
+          Combining the state of the art and my programming knowledge, I am able
+          to build a professional and interactive website. <br /> <br /> When
+          I'm not in front of a computer screen, I'm probably playing game with
+          my friends, watch some show and movie.
+        </p>
+      </div>
+    </section>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/header'
 import Navbar from '../components/navbar'
+import Story from '../components/story'
 
 export default function Home() {
   return (
     <div className='w-full bg-white'>
       <Navbar />
       <Header />
+      <Story />
     </div>
   )
 }
